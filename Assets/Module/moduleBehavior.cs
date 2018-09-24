0184f297eff7ad4d03f93ef7cd47290fff6cd3c2
tacio medeiros <tacio@narsvera.com>
2018-09-24 01:22:06 -0300

    first commit

---
+++ Assets/Module/moduleBehavior.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moduleBehavior : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("Hi!");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
